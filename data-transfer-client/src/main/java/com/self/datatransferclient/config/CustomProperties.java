5ce9abb4d4643f7fc867de2ea8f380b1197212fe
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-09 11:35:36 +0800

    读取配置上传

---
+++ data-transfer-client/src/main/java/com/self/datatransferclient/config/CustomProperties.java
@@ -0,0 +1,20 @@
+package com.self.datatransferclient.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义配置
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "custom")
+public class CustomProperties {
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+}
