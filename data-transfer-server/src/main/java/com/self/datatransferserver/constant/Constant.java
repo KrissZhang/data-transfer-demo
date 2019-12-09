31c2548daa05a97959dc8032286ab71491126273
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-09 11:53:29 +0800

    常量类上传

---
+++ data-transfer-server/src/main/java/com/self/datatransferserver/constant/Constant.java
@@ -0,0 +1,13 @@
+package com.self.datatransferserver.constant;
+
+/**
+ * 常量
+ */
+public final class Constant {
+
+    /**
+     * 空字符串
+     */
+    public static final String STR_EMPTY = "";
+
+}
