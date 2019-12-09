d16be6dc26939224c9a0a2384993f2c2c1c06f25
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-09 11:48:32 +0800

    启动类上传

---
+++ data-transfer-client/src/main/java/com/self/datatransferclient/config/InitApplicationRunner.java
@@ -0,0 +1,18 @@
+package com.self.datatransferclient.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 启动初始化
+ */
+@Component
+public class InitApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("启动成功");
+    }
+
+}
