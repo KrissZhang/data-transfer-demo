e719462e196309a39cabf5e8d6ab8d29f555289f
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-10 21:24:01 +0800

    集成Quartz

---
+++ data-transfer-server/src/main/java/com/self/datatransferserver/schedule/TransferTestSchedule.java
@@ -0,0 +1,94 @@
+package com.self.datatransferserver.schedule;
+
+import com.self.datatransferserver.schedule.execute.TransferTestJobLogic;
+import com.self.datatransferserver.schedule.service.QuartzJobService;
+import com.self.datatransferserver.schedule.task.TaskBuilder;
+import org.quartz.JobKey;
+import org.quartz.SchedulerException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 数据传输测试定时任务
+ */
+@Service
+public class TransferTestSchedule {
+
+    @Resource
+    private QuartzJobService quartzJobService;
+
+    //初始化JobKey
+    private final JobKey jobKey = JobKey.jobKey("TransferTest", "Group1");
+
+    /**
+     * 启动任务
+     * @return 启动结果提示
+     * @throws SchedulerException SchedulerException
+     */
+    public String startTransferTestJob() throws SchedulerException {
+        TaskBuilder task = TaskBuilder.builder()
+                .jobKey(jobKey)
+                .cronExpression("0 */1 * * * ?")
+                .jobClass(TransferTestJobLogic.class)
+                .description("数据传输测试任务")
+                .build();
+
+        quartzJobService.scheduleJob(task);
+
+        return "start transfer test job success";
+    }
+
+    /**
+     * 暂停任务
+     * @return 暂停结果描述
+     * @throws SchedulerException SchedulerException
+     */
+    public String pauseTransferTestJob() throws SchedulerException {
+        quartzJobService.pauseJob(jobKey);
+
+        return "pause transfer test job success";
+    }
+
+    /**
+     * 恢复任务
+     * @return 恢复结果描述
+     * @throws SchedulerException SchedulerException
+     */
+    public String resumeTransferTestJob() throws SchedulerException {
+        quartzJobService.resumeJob(jobKey);
+
+        return "resume transfer test job success";
+    }
+
+    /**
+     * 删除任务
+     * @return 删除结果描述
+     * @throws SchedulerException SchedulerException
+     */
+    public String deleteTransferTestJob() throws SchedulerException {
+        quartzJobService.deleteJob(jobKey);
+
+        return "delete transfer test job success";
+    }
+
+    /**
+     * 修改任务cron表达式
+     * @return 修改结果描述
+     */
+    public String modifyTransferTestJobCron(){
+        TaskBuilder modifyCronTask = TaskBuilder.builder()
+                .jobKey(jobKey)
+                .cronExpression("0 */2 * * * ?")
+                .jobClass(TransferTestJobLogic.class)
+                .description("数据传输测试任务")
+                .build();
+
+        if(quartzJobService.modifyJobCron(modifyCronTask)){
+            return "modify transfer test job success";
+        }else {
+            return "modify transfer test job fail";
+        }
+    }
+
+}
---
+++ data-transfer-server/src/main/java/com/self/datatransferserver/schedule/task/TaskBuilder.java
@@ -0,0 +1,46 @@
+package com.self.datatransferserver.schedule.task;
+
+import lombok.Builder;
+import lombok.Data;
+import org.quartz.Job;
+import org.quartz.JobKey;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+/**
+ * 定时任务构造器
+ */
+@Data
+@Builder
+public class TaskBuilder {
+
+    /**
+     * 定时任务的名字和分组名
+     */
+    @NotNull(message = "定时任务的名字和组名不能为空")
+    private JobKey jobKey;
+
+    /**
+     * 定时任务描述
+     */
+    private String description;
+
+    /**
+     * cron表达式
+     */
+    @NotNull(message = "定时任务执行cron不能为空")
+    private String cronExpression;
+
+    /**
+     * 定时任务的元数据
+     */
+    private Map<?, ?> jobDataMap;
+
+    /**
+     * 定时任务逻辑执行类
+     */
+    @NotNull(message = "定时任务具体执行逻辑类不能为空")
+    private Class<? extends Job> jobClass;
+
+}
