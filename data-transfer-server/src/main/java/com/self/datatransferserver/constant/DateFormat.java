e4d90120cd4ea6e467739ca20cbe7cb917ba16fb
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-12 10:02:25 +0800

    日期工具类上传

---
+++ data-transfer-server/src/main/java/com/self/datatransferserver/constant/DateFormat.java
@@ -0,0 +1,48 @@
+package com.self.datatransferserver.constant;
+
+/**
+ * 日期格式
+ */
+public final class DateFormat {
+
+    /**
+     * 精确到三位毫秒
+     */
+    public static final String FORMAT_YYYYMMDD_HHMMSS_SSS = "yyyyMMddHHmmssSSS";
+
+    /**
+     * 精确到天(中横线分割)
+     */
+    public static final String FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
+
+    /**
+     * 精确到天
+     */
+    public static final String FORMAT_YYYYMMDD = "yyyyMMdd";
+
+    /**
+     * 精确到三位毫秒(中横线分割日期)
+     */
+    public static final String FORMAT_YYYY_MM_DD_HHMMSSSSS = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    /**
+     * 精确到秒
+     */
+    public static final String FORMAT_YYYY_MM_DD_HHMMSS = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 精确到秒(T分割)
+     */
+    public static final String FORMAT_YYYY_MM_DD_T_HHMMSS = "yyyy-MM-dd'T'HH:mm:ss";
+
+    /**
+     * 精确到天(斜杠分割)
+     */
+    public static final String FORMAT_YYYY_MM_DD_SLASH = "yyyy/MM/dd";
+
+    /**
+     * 东八区
+     */
+    public static final String FORMAT_GMT_8 = "GMT+8";
+
+}
