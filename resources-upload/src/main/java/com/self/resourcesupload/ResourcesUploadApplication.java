e807de6bdb78ffc2f1e8e34512223b2a2f40e387
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2019-12-13 20:26:49 +0800

    文件服务器上传子项目

---
+++ resources-upload/src/main/java/com/self/resourcesupload/ResourcesUploadApplication.java
@@ -0,0 +1,13 @@
+package com.self.resourcesupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourcesUploadApplication.class, args);
+    }
+
+}
---
+++ resources-upload/src/test/java/com/self/resourcesupload/ResourcesUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.resourcesupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
