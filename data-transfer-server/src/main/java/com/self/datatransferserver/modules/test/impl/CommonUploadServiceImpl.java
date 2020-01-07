e9dd3bf15036f162e63ffbfb2ce9e09f4695aa1a
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2020-01-07 17:21:18 +0800

    上传接收修改

--- data-transfer-server/src/main/java/com/self/datatransferserver/modules/test/impl/CommonUploadServiceImpl.java
+++ data-transfer-server/src/main/java/com/self/datatransferserver/modules/test/impl/CommonUploadServiceImpl.java
@@ -14,8 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
