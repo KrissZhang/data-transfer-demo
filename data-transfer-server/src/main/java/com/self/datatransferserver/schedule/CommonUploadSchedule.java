a585e1a27898626a8b9747b07e85d39e2e3df512
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2020-01-07 18:15:50 +0800

    调整

--- data-transfer-server/src/main/java/com/self/datatransferserver/schedule/CommonUploadSchedule.java
+++ data-transfer-server/src/main/java/com/self/datatransferserver/schedule/CommonUploadSchedule.java
@@ -1,7 +1,10 @@
 package com.self.datatransferserver.schedule;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.self.datatransferserver.config.CustomProperties;
 import com.self.datatransferserver.util.FileUtil;
+import com.self.datatransferserver.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 通用上传文件定时处理任务
@@ -45,7 +51,30 @@ public class CommonUploadSchedule {
      */
     @Scheduled(cron = "0 */1 * * * ?")
     public void sync(){
+        //遍历接收文件目录
+        File[] totalFiles = new File(customProperties.getDirRoot() + customProperties.getDirTemp()).listFiles();
+        List<File> list = Arrays.asList(totalFiles);
 
+        if(list != null && list.size() > 0){
+            list.stream().forEach(file -> {
+                JSONArray array = JsonUtil.readJsonFile(file, StandardCharsets.UTF_8);
+
+                //解析失败则删除文件
+                if(array.size() < 1){
+                    file.delete();
+                    return;
+                }
+
+                for (Object obj : array) {
+                    if(obj instanceof JSONObject){
+                        JSONObject jobj = (JSONObject)obj;
+
+                        //TODO--查询数据库判断数据是否已经保存,若未保存则保存数据并移动文件到保存成功目录,若已保存则直接删除文件
+
+                    }
+                }
+            });
+        }
     }
 
 }
