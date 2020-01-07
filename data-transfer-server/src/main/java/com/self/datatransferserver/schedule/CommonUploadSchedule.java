3057e04e19fdad50bbcaf003fa65cdf69035b129
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2020-01-07 17:30:01 +0800

    添加日志

--- data-transfer-server/src/main/java/com/self/datatransferserver/schedule/CommonUploadSchedule.java
+++ data-transfer-server/src/main/java/com/self/datatransferserver/schedule/CommonUploadSchedule.java
@@ -1,5 +1,6 @@
 package com.self.datatransferserver.schedule;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 /**
  * 通用上传文件定时处理任务
  */
+@Slf4j
 @Component
 @EnableScheduling
 public class CommonUploadSchedule {
